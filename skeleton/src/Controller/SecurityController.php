5a989522d2be66cf241030407c6bb0061bf848cb
Thiry Prune <prune.thiry@hotmail.com>
2025-09-29 15:43:20 +0200

    feat: redirect to home page

--- skeleton/src/Controller/SecurityController.php
+++ skeleton/src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SecurityController extends AbstractController
 {
@@ -28,5 +29,11 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/', name: 'app_index')]
+    public function goTo()
+    {
+        return $this->redirectToRoute('app_home');
     }
 }
